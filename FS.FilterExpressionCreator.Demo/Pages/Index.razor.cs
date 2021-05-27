d98fea2c55a702931b998ea45155f544436d9728
Florian Schick <florian.schick@schick-software.de>
2021-05-27 16:26:05 +0200

    README added.

---
+++ FS.FilterExpressionCreator.Demo/Pages/Index.razor.cs
@@ -0,0 +1,35 @@
+﻿#pragma warning disable 1591
+using FS.FilterExpressionCreator.Demo.Extensions;
+using Markdig;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Threading.Tasks;
+
+namespace FS.FilterExpressionCreator.Demo.Pages
+{
+    public class IndexPage : ComponentBase
+    {
+        private const string README_FILE = "README.md";
+
+        protected string ReadmeMarkup = string.Empty;
+
+        [Inject] private IWebHostEnvironment WebHostEnvironment { get; set; }
+        [Inject] private NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            var readmePath = System.IO.Path.Combine(WebHostEnvironment.GetWebRootPath(), README_FILE);
+            if (!System.IO.File.Exists(readmePath))
+                readmePath = System.IO.Path.Combine(WebHostEnvironment.ContentRootPath, "..", README_FILE);
+
+            var pipeline = new MarkdownPipelineBuilder().UseSoftlineBreakAsHardlineBreak().Build();
+            var uri = new Uri(NavigationManager.BaseUri);
+            ReadmeMarkup = Markdown
+                .ToHtml(await System.IO.File.ReadAllTextAsync(readmePath), pipeline)
+                .Replace("https://filterexpressioncreator.schick-software.de", $"{uri.Scheme}://{uri.Authority}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# FilterExpressionCreator
+
+Dynamically creates expressions to filter collections of IEnumerable/IQueryable
+
+## Demo ##
+
+[https://filterexpressioncreator.schick-software.de/demo](https://filterexpressioncreator.schick-software.de/demo)
+
