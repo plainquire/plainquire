f499ac8b1bac0eda2aaa6828de2f3a0ce8163874
Florian Schick <florian.schick@schick-software.de>
2021-07-10 11:00:48 +0200

    Link to API added to demo page.

---
+++ FS.FilterExpressionCreator.Demo/Shared/NavMenu.razor.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable 1591
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
+
+namespace FS.FilterExpressionCreator.Demo.Shared
+{
+    public class NavMenuComponent : ComponentBase
+    {
+        [Inject] private NavigationManager NavigationManager { get; set; }
+
+        protected string OpeApiLink = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+
+            var uri = new Uri(NavigationManager.BaseUri);
+            OpeApiLink = $"{uri.Scheme}://{uri.Authority}/openapi";
+        }
+    }
+}
