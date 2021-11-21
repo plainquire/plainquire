a8e663ad82becd2bd3ff69b5a99648d46758388b
Florian Schick <florian.schick@schick-software.de>
2021-11-21 18:25:52 +0100

    DateTimeSpan equality and comparison functions added.

---
+++ FS.FilterExpressionCreator.Tests/Tests/DateTimeSpanTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using FS.FilterExpressionCreator.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FS.FilterExpressionCreator.Tests.Tests
+{
+    [TestClass]
+    public class DateTimeSpanTests
+    {
+        [TestMethod]
+        public void WhenDateTimeSpansComparedForEquality_ResultMatchExpected()
+        {
+            var spanA1 = new DateTimeSpan(new DateTimeOffset(2000, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var spanA2 = new DateTimeSpan(new DateTimeOffset(2000, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var spanB = new DateTimeSpan(new DateTimeOffset(2010, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var spanDefault = new DateTimeSpan(DateTimeOffset.MinValue, DateTimeOffset.MinValue);
+
+            spanA1.Should().Be(spanA2);
+            spanA2.Should().NotBe(spanB);
+            spanDefault.Should().Be(default(DateTimeSpan));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeSpansIntersects_IntersectReturnsTrue()
+        {
+            var earlySpan = new DateTimeSpan(new DateTimeOffset(2000, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var laterSpan = new DateTimeSpan(new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2010, 12, 31, 0, 0, 0, TimeSpan.Zero));
+
+            earlySpan.Should().Match<DateTimeSpan>(early => early.Intersect(laterSpan));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeSpansNotIntersects_IntersectReturnsFalse()
+        {
+            var earlySpan = new DateTimeSpan(new DateTimeOffset(2000, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var laterSpan = new DateTimeSpan(new DateTimeOffset(2010, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2010, 12, 31, 0, 0, 0, TimeSpan.Zero));
+
+            earlySpan.Should().Match<DateTimeSpan>(early => !early.Intersect(laterSpan));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeSpanContainsOther_ContainsReturnsTrue()
+        {
+            var outer = new DateTimeSpan(new DateTimeOffset(2000, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var inner = new DateTimeSpan(new DateTimeOffset(2000, 02, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 11, 30, 0, 0, 0, TimeSpan.Zero));
+
+            outer.Should().Match<DateTimeSpan>(o => o.Contains(inner));
+            inner.Should().Match<DateTimeSpan>(i => !i.Contains(outer));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeSpansNotIntersects_TheyDoesNotContainsEachOther()
+        {
+            var outer = new DateTimeSpan(new DateTimeOffset(2000, 01, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2000, 12, 31, 0, 0, 0, TimeSpan.Zero));
+            var inner = new DateTimeSpan(new DateTimeOffset(2000, 02, 01, 0, 0, 0, TimeSpan.Zero), new DateTimeOffset(2010, 12, 31, 0, 0, 0, TimeSpan.Zero));
+
+            outer.Should().Match<DateTimeSpan>(o => !o.Contains(inner));
+            inner.Should().Match<DateTimeSpan>(i => !i.Contains(outer));
+        }
+    }
+}
--- FS.FilterExpressionCreator/Models/DateTimeSpan.cs
+++ FS.FilterExpressionCreator/Models/DateTimeSpan.cs
@@ -5,7 +5,7 @@ namespace FS.FilterExpressionCreator.Models
     /// <summary>
     /// Represents a date time range for filtering
     /// </summary>
-    public struct DateTimeSpan
+    public struct DateTimeSpan : IEquatable<DateTimeSpan>
     {
         /// <summary>
         /// Gets or sets the start of range.
@@ -34,6 +34,55 @@ namespace FS.FilterExpressionCreator.Models
         public TimeSpan Duration => End - Start;
 
         /// <inheritdoc />
-        public override string ToString() => $"{Start:o}_{End:o}";
+        public override string ToString()
+            => $"{Start:o}_{End:o}";
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+            => obj is DateTimeSpan span && Equals(span);
+
+        /// <inheritdoc />
+        public bool Equals(DateTimeSpan other)
+            => Start.Equals(other.Start) && End.Equals(other.End);
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+            => HashCode.Combine(Start, End);
+
+        /// <summary>
+        /// Implements the == operator.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        public static bool operator ==(DateTimeSpan left, DateTimeSpan right) => left.Equals(right);
+
+        /// <summary>
+        /// Implements the != operator.
+        /// </summary>
+        /// <param name="left">The left.</param>
+        /// <param name="right">The right.</param>
+        public static bool operator !=(DateTimeSpan left, DateTimeSpan right) => !(left == right);
+
+        /// <summary>
+        /// Checks if the given <see cref="DateTimeSpan"/> intersects with this one.
+        /// </summary>
+        /// <param name="other">The <see cref="DateTimeSpan"/> to check intersection with.</param>
+        public bool Intersect(DateTimeSpan other)
+        {
+            var thisStartIsLowerThanRhsEnd = Start.CompareTo(other.End) <= 0;
+            var rhsStartIsLowerThanThisEnd = other.Start.CompareTo(End) <= 0;
+            return thisStartIsLowerThanRhsEnd && rhsStartIsLowerThanThisEnd;
+        }
+
+        /// <summary>
+        /// Determines whether this instance contains the object.
+        /// </summary>
+        /// <param name="other">The <see cref="DateTimeSpan"/> to check containment with.</param>
+        public bool Contains(DateTimeSpan other)
+        {
+            var thisStartIsLowerThanRhsStart = Start.CompareTo(other.Start) <= 0;
+            var thisEndIsGreaterThanRhsEnd = End.CompareTo(other.End) >= 0;
+            return thisStartIsLowerThanRhsStart && thisEndIsGreaterThanRhsEnd;
+        }
     }
 }
