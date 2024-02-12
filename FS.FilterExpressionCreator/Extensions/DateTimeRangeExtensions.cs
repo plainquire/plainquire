cb32871fba1bed95728b2ee789e89eea9249dcff
Florian Schick <florian.schick@schick-software.de>
2024-02-12 16:33:52 +0100

    Nullable annotation fixed.

--- FS.FilterExpressionCreator/Extensions/DateTimeRangeExtensions.cs
+++ FS.FilterExpressionCreator/Extensions/DateTimeRangeExtensions.cs
@@ -51,7 +51,7 @@ public static class DateTimeRangeExtensions
     /// <param name="value">The string to convert.</param>
     /// <param name="now">Value used for 'now' when parsing relative date/time values (e.g. one-week-ago).</param>
     /// <param name="cultureInfo">The culture to use when parsing.</param>
-    public static Range<DateTimeOffset> ConvertStringToDateTimeRange(this string value, DateTimeOffset now, CultureInfo? cultureInfo = null)
+    public static Range<DateTimeOffset> ConvertStringToDateTimeRange(this string? value, DateTimeOffset now, CultureInfo? cultureInfo = null)
     {
         if (TryConvertStringToDateTimeRange(value, now, out var dateTimeRange, cultureInfo))
             return dateTimeRange;
