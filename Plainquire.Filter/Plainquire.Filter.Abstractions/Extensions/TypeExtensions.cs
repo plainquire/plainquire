2f5b44a75685287014533d3613b6ec5c7582eaf7
Florian Schick <florian.schick@schick-software.de>
2024-04-21 16:13:02 +0200

    Filter name handling fixed.

---
+++ Plainquire.Filter/Plainquire.Filter.Abstractions/Extensions/TypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Plainquire.Filter.Abstractions;
+
+internal static class TypeExtensions
+{
+    /// <summary>
+    /// Expands the name of the type in the form of <c>Dictionary{int, string}</c> => <c>DictionaryInt32String</c>.
+    /// </summary>
+    /// <param name="type">The type to get the name for.</param>
+    public static string? ExpandTypeName(this Type? type)
+    {
+        if (type == null)
+            return null;
+
+        if (!type.IsGenericType)
+            return type.Name;
+
+        if (type.IsGenericTypeDefinition)
+            return type.Name.Remove(type.Name.IndexOf('`'));
+
+        // If type is a generic type, expand the type name
+        var genericTypeName = ExpandTypeName(type.GetGenericTypeDefinition());
+        var genericArgumentNames = type.GetGenericArguments().Select(ExpandTypeName);
+        return $"{genericTypeName}{string.Join("", genericArgumentNames)}";
+    }
+}
--- Plainquire.Filter/Plainquire.Filter.Abstractions/Properties/InternalsVisibleTo.cs
+++ Plainquire.Filter/Plainquire.Filter.Abstractions/Properties/InternalsVisibleTo.cs
@@ -1,3 +1,6 @@
 ﻿[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Plainquire.Filter")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Plainquire.Page.Mvc")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Plainquire.Page.Swashbuckle")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Plainquire.Sort")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Plainquire.Sort.Mvc")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Plainquire.Sort.Swashbuckle")]
